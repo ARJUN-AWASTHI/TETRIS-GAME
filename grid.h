7f3e537bf3ace25c0a1ef5b7b9d4cb9720fb9796
ARJUN-AWASTHI <arjunawasthi685@gmail.com>
2025-03-01 17:58:46 +0530

    Rest of the game

--- grid.h
+++ grid.h
@@ -1,20 +1,25 @@
 #pragma once
+#include <raylib.h>
+
 #include <vector>
-#include<raylib.h>
 
+class Grid {
+ public:
+  Grid();
+  void Initialize();
+  void Print();
+  void Draw();
+  bool IsCellOutside(int row, int column);
+  bool IsCellEmtpy(int row, int column);
+	int ClearFullRows();
+  int grid[20][10];
 
-class Grid{
-public:
-    Grid();
-    void Initialize();
-    void Print();
-    void Draw();
-    int grid[20][10];
-    
-private:
-   
-    int numRows;
-    int numCols;
-    int cellSize;
-    std::vector<Color> colors;
+ private:
+  bool IsRowFull(int row);
+  void ClearRow(int row);
+	void MoveRowDown(int row , int numRows);
+  int numRows;
+  int numCols;
+  int cellSize;
+  std::vector<Color> colors;
 };
--- main.cpp
+++ main.cpp
@@ -1,24 +1,48 @@
 #include <raylib.h>
-#include <iostream>
-#include "grid.h"
-#include "blocks.cpp"
-int main(){
-    Color darkBlue = {44,44,127,255};
-    InitWindow(300,600,"TETRIS");
-    SetTargetFPS(60);
-    Grid grid;
-    TBlock block;
-    grid.Print(); 
-    while(!WindowShouldClose()){
-        BeginDrawing();
-        ClearBackground(darkBlue);
-        grid.Draw();
-        block.Draw();
+#include<iostream>
+#include "color.h"
+#include "game.h"
 
-        EndDrawing();
+double lastUpdateTime = 0;
+bool EventTriggered(double interval) {
+  double currentTime = GetTime();
+  if (currentTime - lastUpdateTime >= interval) {
+    lastUpdateTime = currentTime;
+    return true;
+  }
+  return false;
+}
+
+int main() {
+  InitWindow(500, 620, "TETRIS");
+  SetTargetFPS(100);
+
+  Font font = LoadFontEx("font/FiraCodeNerdFontPropo-Regular.ttf", 64, 0, 0);
+
+  Game game;
+
+  while (!WindowShouldClose()) {
+    game.HandleInput();
+    if (EventTriggered(0.2)) game.MoveBlockDown();
+    BeginDrawing();
+    ClearBackground(darkBlue);
+    DrawTextEx(font, "SCORE", {365, 15}, 38, 2, WHITE);
+    DrawTextEx(font, "NEXT", {370, 175}, 38, 2, WHITE);
+    if (game.gameOver) {
+      DrawTextEx(font, "GAME OVER", {310, 450}, 38, 2, WHITE);
     }
-    
-    CloseWindow();
-    return 0; 
+    DrawRectangleRounded({320, 55, 170, 60}, 0.3, 6, lightBlue);
+    char scoreText[10];
+    std::sprintf(scoreText,"%d",game.score);
+    Vector2 textSize = MeasureTextEx(font,scoreText,38,2);
+    DrawTextEx(font, scoreText, {320 + (170-textSize.x)/2, 65}, 38, 2, WHITE);
+    DrawRectangleRounded({320, 215, 170, 180}, 0.3, 6, lightBlue);
 
+    game.Draw();
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
 }
